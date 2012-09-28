ff132636a536e8f4b19a608d3293f2251f7e5f0a
timjstewart <tims@ecollege.com>
2012-09-28 10:52:38 -0600

    Comments.

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import mongo_corpora
 
 from nltk_extractor import TagExtractor
 
+# Useful Resource for POS tags: http://www.ai.mit.edu/courses/6.863/tagdef.html
+
 # The tag extraction process is data-driven.  Here is the data that drives it.  
 # It specifies which database(s), collections, and fields to operate upon.
 databases = {
@@ -33,6 +35,8 @@ databases = {
   }
 }
 
+# Different parts of speech have different priorities.  E.g. a Noun Phrase (NP) is the most
+# important because it represents a concept.
 PRIORITY = { 
   "VB": 2,
   "NP": 3
@@ -42,7 +46,7 @@ def get_priority(pos):
   if PRIORITY.get(pos):
     return PRIORITY[pos]
   else:
-    sys.stderr.write('No Priority for POS: {0}\n'.format(pos))
+    sys.stderr.write('No Priority for POS: {0}.  Priority will be set to zero.\n'.format(pos))
     return 0
 
 def tags_to_hash(tags):
@@ -55,6 +59,8 @@ def tags_to_hash(tags):
   } for tag in tags]
 
 def main():
+  """Main entry point of the system."""
+
   # Create the TagExtractor, a wrapper around the NLTK toolkit.
   extractor = TagExtractor()
 
@@ -73,11 +79,13 @@ def main():
       corpora = mongo_corpora.get_corpora(database = database_name, collection = collection)
 
       for corpus in corpora.get_all():
+        # Get the text we're going to extract tags from.
         text = corpus.get_text(text_field) 
         if text:
           tags = extractor.extract_tags(text)
           for tag in tags:
             print str(tag)
+          # Put the extracted tags back into the database in the same record but different field.
           corpus.put_tags(tags_field, tags_to_hash(tags))
           corpus.save()
 
